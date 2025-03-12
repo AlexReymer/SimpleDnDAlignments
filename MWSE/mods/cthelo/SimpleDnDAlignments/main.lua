899cbae179b1acbd9a08137aab4cdb8a3d79a1bc
AlexReymer <alexreymer@gmail.com>
2025-03-12 16:55:19 -0400

    Added remaining alignments, sorted alignments before adding them to the alignments list block

--- MWSE/mods/cthelo/SimpleDnDAlignments/main.lua
+++ MWSE/mods/cthelo/SimpleDnDAlignments/main.lua
@@ -7,6 +7,7 @@ local alignmentDescriptionTextId = "AlignmentDescriptionBlockText"
 local alignmentsTable = {
     ["lawfulGood"] = {
         id = "lawfulGood",
+        order = 0,
         name = "Lawful Good",
         description = (
             "A lawful good character typically acts with compassion and always with honor " ..
@@ -16,6 +17,7 @@ local alignmentsTable = {
     },
     ["neutralGood"] = {
         id = "neutralGood",
+        order = 2,
         name = "Neutral Good",
         description = (
             "A neutral good character typically acts altruistically, without regard for or " .. 
@@ -23,8 +25,81 @@ local alignmentsTable = {
             "with cooperating with lawful officials, but does not feel beholden to them. In the event that " ..
             "doing the right thing requires the bending or breaking of rules, they do not suffer the same " ..
             "inner conflict that a lawful good character would."
-        ) 
-    }
+        )
+    },
+    ["chaoticGood"] = {
+        id = "chaoticGood",
+        order = 4,
+        name = "Chaotic Good",
+        description = (
+            "A chaotic good character does whatever is necessary to bring about change for the better, " .. 
+            "disdains bureaucratic organizations that get in the way of social improvement, and places a " ..
+            "high value on personal freedom, not only for oneself but for others as well. Chaotic good " ..
+            "characters usually intend to do the right thing, but their methods are generally disorganized " ..
+            "and often out of sync with the rest of society."
+        )
+    },
+    ["lawfulNeutral"] = {
+        id = "lawfulNeutral",
+        order = 10,
+        name = "Lawful Neutral",
+        description = (
+            "A lawful neutral character typically believes strongly in lawful concepts such as honor, order, " ..
+            "rules, and tradition, but often follows a personal code in addition to, or even in preference to, " ..
+            "one set down by a benevolent authority."
+        )
+    },
+    ["trueNeutral"] = {
+        id = "trueNeutral",
+        order = 12,
+        name = "True Neutral",
+        description = (
+            "A true neutral character is neutral on both axes and tends not to feel strongly towards any alignment, " ..
+            "or actively seeks their balance."
+        )
+    },
+    ["chaoticNeutral"] = {
+        id = "chaoticNeutral",
+        order = 14,
+        name = "Chaotic Neutral",
+        description = (
+            "A chaotic neutral character is an individualist who follows their own heart and generally shirks rules and " ..
+            "traditions. Although chaotic neutral characters promote the ideals of freedom, it is their own freedom that " ..
+            "comes first; good and evil come second to their need to be free."
+        )
+    },
+    ["lawfulEvil"] = {
+        id = "lawfulEvil",
+        order = 20,
+        name = "Lawful Evil",
+        description = (
+            "A lawful evil character sees a well-ordered system as being necessary to fulfill their own personal wants" ..
+            " and needs, using these systems to further their power and influence."
+        )
+    },
+    ["neutralEvil"] = {
+        id = "neutralEvil",
+        order = 22,
+        name = "Neutral Evil",
+        description = (
+            "A neutral evil character is typically selfish and has no qualms about turning on allies-of-the-moment, and " ..
+            " usually makes allies primarily to further their own goals. A neutral evil character has no compunctions " ..
+            "about harming others to get what they want, but neither will they go out of their way to cause carnage or " ..
+            "mayhem when they see no direct benefit for themselves. Another valid interpretation of neutral evil holds " ..
+            "up evil as an ideal, doing evil for evil's sake and trying to spread its influence."
+        )
+    },
+    ["chaoticEvil"] = {
+        id = "chaoticEvil",
+        order = 24,
+        name = "Chaotic Evil",
+        description = (
+            "A chaotic evil character tends to have no respect for rules, other people's lives, or anything but their own " ..
+            "desires, which are typically selfish and cruel. They set a high value on personal freedom, but do not have " ..
+            "much regard for the lives or freedom of other people. Chaotic evil characters do not work well in groups " ..
+            "because they resent being given orders and usually do not behave themselves unless there is no alternative."
+        )
+    },
 }
 
 local selectedAlignmentId = "none"
@@ -157,7 +232,9 @@ local function createAlignmentMenu(e)
     alignmentListBlock.paddingAllSides = 4
     alignmentListBlock.borderRight = 6
 
-    for _, alignment in next,alignmentsTable do
+    local sortedAlignments = table.values(alignmentsTable, function(a, b) return a.order < b.order end)
+
+    for _, alignment in pairs(sortedAlignments) do
         local alignmentButton = alignmentListBlock:createTextSelect{ id = "alignmentButton", text = alignment.name }
         alignmentButton.autoHeight = true
         alignmentButton.widthProportional = 1.0
